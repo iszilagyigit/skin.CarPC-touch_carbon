dfdb85c7c566a4a1e350610ce39fe3cd0da0eeb2
iszilagyigit <istvan.szilagyi@t-online.de>
2016-08-08 04:09:42 +0000

    update-skin.py with full path

--- update-skin.py
+++ update-skin.py
@@ -1,5 +1,5 @@
 import subprocess
 
 print "start update skin"
-subprocess.call(["/usr/bin/git", "pull"])
+subprocess.call(["/usr/bin/git", "--git-dir=/home/pi/.kodi/addons/skin.CarPC-touch_carbon/.git", "--work-tree=/home/pi/.kodi/addons/skin.CarPC-touch_carbon" ,"pull"])
 print "end update skin"
